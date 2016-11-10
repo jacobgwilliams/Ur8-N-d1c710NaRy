dc87be9fcbf3074942614beda039602c3827897b
Jacob Williams <email": null>
2016-11-10 18:47:39 -0500

    L33t and urban dictionaries work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,5 +12,24 @@ post '/search' do
   }
   @term = response.body["list"][0]["word"]
   @definition = response.body["list"][0]["definition"]
+
+  @l33t_term = params[:term]
+  @l33t_term = @l33t_term.split.join("+")
+  response = Unirest.get "https://montanaflynn-l33t-sp34k.p.mashape.com/encode?text=#{@l33t_term}",
+  headers:{
+    "X-Mashape-Key" => "lXGPLVSl4Wmshz5shjSmDsGIqYRbp1twZxWjsnzU4ZoARA8RtB",
+    "Accept" => "text/plain"
+  }
+  @l33t_term = response.body
+
+  @l33t_definition = @definition
+  @l33t_definition = @l33t_definition.split.join("+")
+  response = Unirest.get "https://montanaflynn-l33t-sp34k.p.mashape.com/encode?text=#{@l33t_definition}",
+  headers:{
+    "X-Mashape-Key" => "lXGPLVSl4Wmshz5shjSmDsGIqYRbp1twZxWjsnzU4ZoARA8RtB",
+    "Accept" => "text/plain"
+  }
+
+  @l33t_definition = response.body
   erb :'index'
 end
