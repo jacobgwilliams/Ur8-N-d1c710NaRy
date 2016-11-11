a969355c789ad0d6c2342f248b4e5c740f4a0de9
Jacob Williams <email": null>
2016-11-10 19:03:50 -0500

    jQuery toggle for l33t to n00b for definitions

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(document).on('click', '#translate_to_noob', function(event){
+    event.preventDefault();
+    // $('#noob_def').
+    $('#noob_def').removeClass('hidden');
+    $('#translate_to_leet').removeClass('hidden');
+    $('#l33t_def').addClass('hidden');
+    $('#translate_to_noob').addClass('hidden');
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
+  $(document).on('click', '#translate_to_leet', function(event){
+    event.preventDefault();
+    // $('#noob_def').
+    $('#noob_def').addClass('hidden');
+    $('#translate_to_leet').addClass('hidden');
+    $('#l33t_def').removeClass('hidden');
+    $('#translate_to_noob').removeClass('hidden');
+  })
 });
