6f63b1f3331c5166f6010cc3318f34831d95d796
Jacob Williams <email": null>
2016-11-11 11:24:49 -0500

    login functional, going to registratoin

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -9,3 +9,18 @@ end
 def require_login
   redirect '/sessions/new' unless logged_in?
 end
+
+def l33t_username
+  require_login
+  username = current_user.username.split.join("+")
+  response = Unirest.get "https://montanaflynn-l33t-sp34k.p.mashape.com/encode?text=#{username}",
+  headers:{
+    "X-Mashape-Key" => "lXGPLVSl4Wmshz5shjSmDsGIqYRbp1twZxWjsnzU4ZoARA8RtB",
+    "Accept" => "text/plain"
+  }
+  response.body
+end
+
+def l33t_greet
+  "w3LC0m3, " + l33t_username
+end
